87c757fd4136d88e415795f2364e62378eb21a9d
kevinrickert@gmail.com <kevinrickert@gmail.com>
2022-11-06 11:18:41 -0500

    commented out hash

--- synology-drive-latest.rb
+++ synology-drive-latest.rb
@@ -1,6 +1,6 @@
 cask "synology-drive-latest" do
     version "3.1.0,12923"
-    sha256 "b54f9c588457d1cd5d8b9eeace070a3111add4e81136e261fde9a6156217955f"
+    # sha256 "b54f9c588457d1cd5d8b9eeace070a3111add4e81136e261fde9a6156217955f"
   
     url "https://global.download.synology.com/download/Utility/SynologyDriveClient/#{version.csv.first}-#{version.csv.second}/Mac/Installer/synology-drive-client-#{version.csv.second}.dmg"
     name "Synology Drive"
